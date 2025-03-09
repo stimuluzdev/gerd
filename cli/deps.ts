c022e14d4e9fe5eb2f7a0d3fb445f287b8b7c312
uriah-dev <otch.dev@gmail.com>
2025-03-09 03:14:53 +0000

    compilation fixes

--- cli/deps.ts
+++ cli/deps.ts
@@ -1,10 +1,10 @@
-export { parse } from "https://deno.land/std@0.200.0/flags/mod.ts";
-export type { Args } from "https://deno.land/std@0.200.0/flags/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.200.0/testing/asserts.ts";
-import figlet from "https://x.nest.land/deno-figlet@0.0.5/mod.js";
-import * as Path from "https://deno.land/std@0.207.0/path/mod.ts";
-import { readline } from "https://deno.land/x/readline@v1.1.0/mod.ts";
-import * as Prompt from "https://deno.land/x/cliffy@v0.25.7/prompt/mod.ts";
+export { parse } from "./vendor/deno.land/std@0.200.0/flags/mod.ts";
+export type { Args } from "./vendor/deno.land/std@0.200.0/flags/mod.ts";
+export { assertEquals } from "./vendor/deno.land/std@0.200.0/testing/asserts.ts";
+import figlet from "./vendor/deno.land/x/deno_figlet@1.0.0/mod.ts";
+import * as Path from "./vendor/deno.land/std@0.207.0/path/mod.ts";
+import { readline } from "./vendor/deno.land/x/readline@v1.1.0/mod.ts";
+import * as Prompt from "./vendor/deno.land/x/cliffy@v0.25.7/prompt/mod.ts";
 
-const fig = figlet as (v: string) => Promise<void>;
+const fig = figlet;
 export { fig, Path, Prompt, readline };
